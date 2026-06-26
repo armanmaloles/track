9635f832c2cfd7d0122f488a7e2f0595a4a7f8ab
armanmaloles <armanmaloles@gmail.com>
2026-06-26 14:24:30 +0800

    redirect fixed

--- web-user/src/main.jsx
+++ web-user/src/main.jsx
@@ -8,7 +8,22 @@ CapacitorApp.addListener("appUrlOpen", ({ url }) => {
   console.log("Deep link received:", url);
 
   if (url.startsWith("trackv2://oauth-callback")) {
-    window.location.href = "/officials/home";
+    // Extract query params from the deep link
+    const urlObj = new URL(url);
+    const token = urlObj.searchParams.get("token");
+    const registrationToken = urlObj.searchParams.get("registration_token");
+    const email = urlObj.searchParams.get("email");
+    
+    // Store auth data for the app to process
+    if (token) {
+      localStorage.setItem("token", token);
+    } else if (registrationToken) {
+      sessionStorage.setItem("registrationToken", registrationToken);
+      sessionStorage.setItem("registrationEmail", email);
+    }
+    
+    // Navigate to app root - AuthContext will authenticate and redirect to /{role}/home
+    window.location.href = "/";
   }
 });
 
